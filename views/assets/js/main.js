7d7dd2d1b14953f678576eacb0fe78245119c6c2
itjuanpablo <jp664165@gmail.com>
2023-06-05 20:50:27 -0300

    MOD: Retirada `scroll top` main.js

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,106 +1,104 @@
 /**
-* Template Name: PhotoFolio
-* Updated: Mar 10 2023 with Bootstrap v5.2.3
-* Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+ * Template Name: PhotoFolio
+ * Updated: Mar 10 2023 with Bootstrap v5.2.3
+ * Template URL: https://bootstrapmade.com/photofolio-bootstrap-photography-website-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
 document.addEventListener('DOMContentLoaded', () => {
-  "use strict";
+  'use strict'
 
   /**
    * Preloader
    */
-  const preloader = document.querySelector('#preloader');
+  const preloader = document.querySelector('#preloader')
   if (preloader) {
     window.addEventListener('load', () => {
       setTimeout(() => {
-        preloader.classList.add('loaded');
-      }, 1000);
+        preloader.classList.add('loaded')
+      }, 1000)
       setTimeout(() => {
-        preloader.remove();
-      }, 2000);
-    });
+        preloader.remove()
+      }, 2000)
+    })
   }
 
   /**
    * Mobile nav toggle
    */
-  const mobileNavShow = document.querySelector('.mobile-nav-show');
-  const mobileNavHide = document.querySelector('.mobile-nav-hide');
+  const mobileNavShow = document.querySelector('.mobile-nav-show')
+  const mobileNavHide = document.querySelector('.mobile-nav-hide')
 
   document.querySelectorAll('.mobile-nav-toggle').forEach(el => {
-    el.addEventListener('click', function(event) {
-      event.preventDefault();
-      mobileNavToogle();
+    el.addEventListener('click', function (event) {
+      event.preventDefault()
+      mobileNavToogle()
     })
-  });
+  })
 
   function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavShow.classList.toggle('d-none');
-    mobileNavHide.classList.toggle('d-none');
+    document.querySelector('body').classList.toggle('mobile-nav-active')
+    mobileNavShow.classList.toggle('d-none')
+    mobileNavHide.classList.toggle('d-none')
   }
 
   /**
    * Hide mobile nav on same-page/hash links
    */
   document.querySelectorAll('#navbar a').forEach(navbarlink => {
+    if (!navbarlink.hash) return
 
-    if (!navbarlink.hash) return;
-
-    let section = document.querySelector(navbarlink.hash);
-    if (!section) return;
+    let section = document.querySelector(navbarlink.hash)
+    if (!section) return
 
     navbarlink.addEventListener('click', () => {
       if (document.querySelector('.mobile-nav-active')) {
-        mobileNavToogle();
+        mobileNavToogle()
       }
-    });
-
-  });
+    })
+  })
 
   /**
    * Toggle mobile nav dropdowns
    */
-  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a');
+  const navDropdowns = document.querySelectorAll('.navbar .dropdown > a')
 
   navDropdowns.forEach(el => {
-    el.addEventListener('click', function(event) {
+    el.addEventListener('click', function (event) {
       if (document.querySelector('.mobile-nav-active')) {
-        event.preventDefault();
-        this.classList.toggle('active');
-        this.nextElementSibling.classList.toggle('dropdown-active');
+        event.preventDefault()
+        this.classList.toggle('active')
+        this.nextElementSibling.classList.toggle('dropdown-active')
 
-        let dropDownIndicator = this.querySelector('.dropdown-indicator');
-        dropDownIndicator.classList.toggle('bi-chevron-up');
-        dropDownIndicator.classList.toggle('bi-chevron-down');
+        let dropDownIndicator = this.querySelector('.dropdown-indicator')
+        dropDownIndicator.classList.toggle('bi-chevron-up')
+        dropDownIndicator.classList.toggle('bi-chevron-down')
       }
     })
-  });
+  })
 
   /**
    * Scroll top button
    */
-  const scrollTop = document.querySelector('.scroll-top');
-  if (scrollTop) {
-    const togglescrollTop = function() {
-      window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
-    }
-    window.addEventListener('load', togglescrollTop);
-    document.addEventListener('scroll', togglescrollTop);
-    scrollTop.addEventListener('click', window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    }));
-  }
+  // const scrollTop = document.querySelector('.scroll-top');
+  // if (scrollTop) {
+  //   const togglescrollTop = function() {
+  //     window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  //   }
+  //   window.addEventListener('load', togglescrollTop);
+  //   document.addEventListener('scroll', togglescrollTop);
+  //   scrollTop.addEventListener('click', window.scrollTo({
+  //     top: 0,
+  //     behavior: 'smooth'
+  //   }));
+  // }
 
   /**
    * Initiate glightbox
    */
   const glightbox = GLightbox({
     selector: '.glightbox'
-  });
+  })
 
   /**
    * Init swiper slider with 1 slide at once in desktop view
@@ -120,9 +118,9 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-prev'
     }
-  });
+  })
 
   /**
    * Init swiper slider with 3 slides at once in desktop view
@@ -142,7 +140,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     navigation: {
       nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-prev'
     },
     breakpoints: {
       320: {
@@ -151,10 +149,10 @@ document.addEventListener('DOMContentLoaded', () => {
       },
 
       1200: {
-        slidesPerView: 3,
+        slidesPerView: 3
       }
     }
-  });
+  })
 
   /**
    * Animation on scroll function and init
@@ -165,10 +163,9 @@ document.addEventListener('DOMContentLoaded', () => {
       easing: 'ease-in-out',
       once: true,
       mirror: false
-    });
+    })
   }
   window.addEventListener('load', () => {
-    aos_init();
-  });
-
-});
+    aos_init()
+  })
+})
